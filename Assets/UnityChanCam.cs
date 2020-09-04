8e64968670162bb4930c5c3e27ac463302642862
hiroto_takizawa <takky0131@gmail.com>
2020-09-04 20:48:08 +0900

    お願いいたします。

---
+++ Assets/UnityChanCam.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanCam : MonoBehaviour
+{
+    private GameObject Unitychan;
+    private GameObject enemy;
+    private GameObject maincamera;
+    private float rotateSpeed = 4.0f;
+    Renderer enemyRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Unitychan = GameObject.Find("unitychan");
+        enemy = GameObject.Find("Ethan");
+        maincamera = Camera.main.gameObject;
+        GameObject enemyObject = GameObject.Find("EthanBody");    
+        enemyRenderer = enemyObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyRenderer.isVisible)
+        {
+            this.transform.LookAt(enemy.transform);
+        }
+        else
+        {
+            rotateCamera();
+        }
+        rotateCamera();
+    }
+
+    private void rotateCamera()
+    {
+        Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed, 0, 0);
+        maincamera.transform.RotateAround(Unitychan.transform.position, Vector3.up, angle.x);
+    }
+
+}
---
+++ Assets/UnityChanCnt.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanCnt : MonoBehaviour
+{
+    
+    private Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            myAnimator.SetBool("Runf Bool", true);
+        }
+        else
+        {
+            myAnimator.SetBool("Runf Bool", false);
+            Debug.Log(myAnimator.GetBool("Runf Bool"));
+        }  
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            myAnimator.SetBool("Back Bool", true);
+        }
+        else
+        {
+            myAnimator.SetBool("Back Bool", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            myAnimator.SetBool("Runl Bool", true);
+        }
+        else
+        {
+            myAnimator.SetBool("Runl Bool", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myAnimator.SetBool("Runr Bool", true);
+        }
+        else
+        {
+            myAnimator.SetBool("Runr Bool", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myAnimator.SetTrigger("JumpTrigger");
+        }      
+
+    }
+}
